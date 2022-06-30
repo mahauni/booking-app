813370c164c6adf33123556919f02df25168c6c5
Lucas Raoni <lucasraoniha@gmail.com>
2022-06-30 19:41:25 -0300

    mid watchalong coding nana

--- main.go
+++ main.go
@@ -1,5 +1,81 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	conferenceName := "Go conference"
+	const conferenceTickets = 50
+	var remainingTickets uint = 50
+	var bookings []string
+
+	fmt.Println("welcome to", conferenceName, "booking aplication")
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend!")
+
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+		// ask for their name
+		fmt.Println("Enter your first name:")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last name:")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email address:")
+		fmt.Scan(&email)
+
+		fmt.Println("Enter your number of tickets:")
+		fmt.Scan(&userTickets)
+
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+		if isValidName && isValidEmail && isValidTicketNumber {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
+
+			fmt.Printf("The whole array: %v\n", bookings)
+			fmt.Printf("The first array: %v\n", bookings[0])
+			fmt.Printf("Slice type: %T\n", bookings)
+			fmt.Printf("Slice length: %v\n", len(bookings))
+
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)
+
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+
+			fmt.Printf("There first names of bookings are : %v.\n", firstNames)
+
+			if remainingTickets == 0 {
+				// end program
+				fmt.Println("Our conference is booked out. Come back next year.")
+				break
+			}
+		} else {
+			if !isValidEmail {
+				fmt.Println("first name or last name you entered is too short.")
+			}
+			if !isValidEmail {
+				fmt.Println("email address you entered doesn't contain the @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("number of tickets you entered is invalid")
+			}
+		}
+	}
+}
+
+func greetUsers() {
+	fmt.Println("Welcome to our conference")
 }
